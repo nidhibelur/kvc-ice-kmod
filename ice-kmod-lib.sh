ca04460e3e79996f47299a91d1e36d982b426388
jianzzha <jianzzha@redhat.com>
2021-09-12 15:50:21 +0000

    update

--- ice-kmod-lib.sh
+++ ice-kmod-lib.sh
@@ -121,10 +121,9 @@ build_kmods() {
 load_kmods() {
     echo "Loading kernel modules using the kernel module container..."
     for module in ${KMOD_NAMES}; do
-            module=${module//-/_} # replace any dashes with underscore
-            kvc_c_run --privileged "$IMAGE" rmmod "${module}"
-            kvc_c_run --privileged "$IMAGE" modprobe "${module}" 
-        fi
+        module=${module//-/_} # replace any dashes with underscore
+        kvc_c_run --privileged "$IMAGE" rmmod "${module}"
+        kvc_c_run --privileged "$IMAGE" modprobe "${module}" 
     done
 }
 
